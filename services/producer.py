b83a244ba64002746834d1e3e374c2ad5ffb0a93
Corey Durthaler <cdurth@gmail.com>
2017-01-25 16:38:53 -0500

    build a queue

--- services/producer.py
+++ services/producer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import pika
 import sys
+import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
@@ -8,12 +9,17 @@ channel = connection.channel()
 
 channel.queue_declare(queue='task_queue', durable=True)
 
-message = ' '.join(sys.argv[1:]) or "https://www.youtube.com/watch?v=6HzyUHxmkg0"
-channel.basic_publish(exchange='',
-                      routing_key='task_queue',
-                      body=message,
-                      properties=pika.BasicProperties(
-                         delivery_mode = 2, # make message persistent
-                      ))
-print(" [x] Sent %r" % message)
+
+with open('songs.json') as songs_file:
+    songs = json.load(songs_file)
+    for song in songs['items']:
+        songID = song['contentDetails']['videoId']
+        message = "https://www.youtube.com/watch?v=" + songID
+        channel.basic_publish(exchange='',
+                              routing_key='task_queue',
+                              body=message,
+                              properties=pika.BasicProperties(
+                                 delivery_mode = 2, # make message persistent
+                              ))
+        print(" [x] Sent %r" % message)
 connection.close()
